09147fe8901206d63400db66553cdca148c9483a
stefanlewis94 <stefan.lewis94@hotmail.co.uk>
2020-12-29 17:19:48 +0000

    skeleton of activation functions added. Function to define which activation function desired has been added.

---
+++ activations.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class ReLU:
+    def __init__(self):
+        pass
+
+    def fprop(self):
+        return np.max(0,1)
+
+    def bprop(self, gradients):
+        if gradients > 0:
+            return 1
+        else:
+            return 0
+
+class Sigmoid:
+    pass
+
+
+def define_activation_function(activation_function):
+
+    available_activation_functions = ["ReLU", "Sigmoid"]
+    assert activation_function in available_activation_functions, \
+            f"Activation function selected is not available. Please choose from {available_activation_functions}"
+
+    if activation_function == "ReLU":
+        activation_function = ReLU()
+    elif activation_function == "Sigmoid":
+        activation_function = Sigmoid()
+
+    return activation_function
