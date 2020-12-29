8e0741392cdf7b1a115038251d1e51734d343c2a
stefanlewis94 <stefan.lewis94@hotmail.co.uk>
2020-12-29 17:21:58 +0000

    two loss functions defined in file. Function added to define the desired loss function

---
+++ loss_functions.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def mean_square_error(prediction, y):
+    loss = np.sqrt((prediction - y)**2)
+    return loss
+
+def cross_entropy_error(prediction, y):
+    loss = -np.sum([prediction[i] * np.log2(y[i]) for i in range(len(y))])
+    return loss
+
+
+def define_loss_function(loss_function_string):
+
+    available_loss_functions = ["MSE", "cross_entropy"]
+    assert loss_function_string in available_loss_functions,\
+        f"Activation function selected is not available. Please choose from {available_loss_functions}"
+
+    if loss_function_string == "MSE":
+        loss_function = mean_square_error()
+    elif loss_function_string == "cross_entropy":
+        loss_function = cross_entropy_error()
+
+    return loss_function
+
+
