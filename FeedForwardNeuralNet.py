2d135d6c60716df8d0cc98e5c764963e8fd0d322
stefanlewis94 <stefan.lewis94@hotmail.co.uk>
2020-12-30 17:17:11 +0000

    Network parameters are now defined and stored in network dict. These are intialised using _get_network_dict().

--- FeedForwardNeuralNet.py
+++ FeedForwardNeuralNet.py
@@ -1,47 +1,52 @@
 import numpy as np
 import loss_functions
 import activations
-"""
-Basic idea behind the Network:
-1. high level network stuff happens in this file - try to make like Keras
-
-"""
-
+import output_layers
 
 
 class FeedForwardNeuralNet:
 
-    def __init__(self, input_dim, output_dim, num_units_per_hidden_layer,
-                 activation_function, loss, regularisation=None):
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        self.num_units_per_hidden_layer = num_units_per_hidden_layer
+    def __init__(self, network_shape, activation_function="ReLU", output_layer=None, loss="cross_entropy", regularisation=None):
+        # network shape is a list containing the number of units per layer. This include the input and output dims.
+        self.network_shape = network_shape
         self.activation_function = activations.define_activation_function(activation_function)
+        self.output_layer = output_layers.define_output_layer(output_layer)
         self.loss = loss_functions.define_loss_function(loss)
-        # TODO: linke regularisation directly into loss function
+        # TODO: link regularisation directly into loss function
         self.regularisation = regularisation
-        self.weights = None
-        self.bias = None
+        self.network_dict = {}
+        self._get_network_dict()
 
 
-    def get_network_dict(self):
+    def _get_network_dict(self):
         """
         Function obtains a dictionary that defines the neural network.
         The dict contains the weights and biases for the network and are updated during the training process.
         """
 
+        for index in range(len(self.network_shape)-1):
+            layer_number = index + 1
+            matrix_input_size, matrix_output_size = self.network_shape[layer_number], self.network_shape[layer_number+1]
+            self.network_dict[f"hidden_layer_{index}"] = [init_weights(matrix_input_size, matrix_output_size),
+                                                     init_biases(matrix_input_size, matrix_output_size)]
+
         # define layers as "hidden_layer_x" : [weight and biases in here]
+
+    def _fprop(self):
         pass
 
 
     def _forward(self, x):
-        network_dict = FeedForwardNeuralNet.get_network_dict()
-        for layer in range(len(self.num_units_per_hidden_layer)):
-            # do fprop in here
-            # fprop, _ =
-            pass
+        output = x
+        for index in range(len(self.network_shape)):
+            layer_number = index + 1
+            weights, biases = self.network_dict[f"hidden_layer_{layer_number}"]
+            output = self._fprop(output, weights, biases)
+            if index != range(len(self.network_shape)) -1 :
+                output = self.activation_function(output)
 
-        output = output_layer(x)
+        # final layer requires different activation function depending on output
+        output = self.output_layer(x)
         return output
 
 
