38d1b1d868618911e4e21c5b28a7d59f595f7b48
stefanlewis94 <stefan.lewis94@hotmail.co.uk>
2021-01-11 14:37:31 +0000

    loss functions are now classes to allow gradient of loss within class

--- loss_functions.py
+++ loss_functions.py
@@ -1,33 +1,37 @@
 import numpy as np
 
 
-class mean_square:
+class SumOfSquares:
 
-    def mean_square_error(self, prediction, y):
-        loss = ((prediction - y)**2) / len(prediction)
-        return loss
-
-    # TODO: need to work out gradients and backprop
-
-class cross_entropy:
-
-    def cross_entropy_error(self, prediction, y):
-        return -np.sum(y * np.log2(prediction))
+    def error(self, prediction, y):
+        # TODO: Add ref to this and cross_entropy
+        return np.mean(np.sum((prediction - y)**2, axis=1)) * 0.5
 
     # TODO: need to work out gradients and backprop
+    def gradients(self, prediction, y):
 
+        return (prediction - y) / prediction.shape[0]
+
+class CrossEntropy:
+
+    def error(self, prediction, y):
+        return -np.mean(np.sum(y * np.log2(prediction)), axis=1)
+
+    # TODO: need to work out gradients and backprop
+    def gradients(self, prediction, y):
+        return -(y / prediction) / prediction.shape[0]
 
 
 def define_loss_function(loss_function_string):
 
-    available_loss_functions = ["mse", "cross_entropy"]
+    available_loss_functions = ["sum_of_squares", "cross_entropy"]
     assert loss_function_string in available_loss_functions,\
         f"Activation function selected is not available. Please choose from {available_loss_functions}"
 
-    if loss_function_string == "mse":
-        loss_function = mean_square
+    if loss_function_string == "sum_of_squares":
+        loss_function = SumOfSquares()
     elif loss_function_string == "cross_entropy":
-        loss_function = cross_entropy
+        loss_function = CrossEntropy()
 
     return loss_function
 
